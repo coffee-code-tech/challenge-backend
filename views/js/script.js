0a94b72fa41506ffa4870b903a356e528ce1c612
marciofao <marcio.lopes.fao@gmail.com>
2023-03-17 20:04:18 -0300

    ensure dom is loaded

--- views/js/script.js
+++ views/js/script.js
@@ -1,36 +1,42 @@
 
-async function getProducts() {
-    let url = 'http://localhost/tester/wp-json/myapp/v1/products?paged=100,1';
-    try {
-        let res = await fetch(url);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
+
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    async function getProducts() {
+        let url = 'http://localhost/tester/wp-json/myapp/v1/products?paged=100,1';
+        try {
+            let res = await fetch(url);
+            return await res.json();
+        } catch (error) {
+            console.log(error);
+        }
     }
-}
 
 
-async function renderProducts() {
+    async function renderProducts() {
 
 
-    let products = await getProducts();
+        let products = await getProducts();
 
-    let html = '';
-    products.forEach(product => {
-        let htmlSegment = `
-                                <div class="single-product">
-                                <a href="${product.productLink}">
-                                    <img src="${product.imageURL}" >
-                                    <h2>${product.title} </h2>
-                                    </a>
-                                </div>
-                            `;
+        let html = '';
+        products.forEach(product => {
+            let htmlSegment = `
+                                    <div class="single-product">
+                                    <a href="${product.productLink}">
+                                        <img src="${product.imageURL}" >
+                                        <h2>${product.title} </h2>
+                                        </a>
+                                    </div>
+                                `;
 
-        html += htmlSegment;
-    });
+            html += htmlSegment;
+        });
 
-    let container = document.querySelector('.product-listing');
-    container.innerHTML = html;
-}
+        let container = document.querySelector('.product-listing');
+        container.innerHTML = html;
+    }
 
-renderProducts();
+    renderProducts();
+
+
+});
