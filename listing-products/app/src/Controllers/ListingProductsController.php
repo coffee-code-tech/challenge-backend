bce6d37bae828a79db0244724f282f04c34d9ed9
Diego Lima <diegolima.sao@gmail.com>
2023-04-12 15:51:21 -0300

    challenge

---
+++ listing-products/app/src/Controllers/ListingProductsController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Controllers;
+
+class ListingProductsController{
+    public function index(){
+        exit('hi');
+    }
+}
---
+++ listing-products/resources/scripts/frontend/index.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+import config from '@config';
+import './vendor/*.js';
+import '@styles/frontend';
+import './spritesvg';
+
+// Your code goes here ...
